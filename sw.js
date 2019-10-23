bf798adf61f029f93f3fab2de3eb0d9d823b1072
RayanSub <rss.321@hotmail.com>
2019-10-23 22:41:04 +0300

    first commit

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-console.log('gg');
 var staticCacheName = "restaurants-static-v1";   //https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
 
 self.addEventListener("install", function(event) {
